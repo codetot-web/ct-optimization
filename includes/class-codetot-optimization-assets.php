5a79fce6f8cf95680ca336604b3d528a3ebb6949
Khoi Nguyen <khoi@codetot.com>
2021-03-04 11:19:40 +0700

    Add support async to lazysizes script

--- includes/class-codetot-optimization-assets.php
+++ includes/class-codetot-optimization-assets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link       https://codetot.com
  * @package    Codetot_Optimization
@@ -48,26 +49,43 @@ class Codetot_Optimization_Assets
       if ($option === 'yes') {
         // Convert yes/no to true/false
         $this->options[$key] = true;
-      } elseif($option === 'no') {
+      } elseif ($option === 'no') {
         $this->options[$key] = false;
       } else {
         $this->options[$key] = $option;
       }
     }
 
+    add_filter('clean_url', array($this, 'add_async_forscript'), 11, 1);
+
     if (!empty($this->options['load_lazysizes_scripts'])) {
       add_action('wp_enqueue_scripts', array($this, 'load_lazysizes_scripts'));
     }
   }
 
-  public function load_lazysizes_scripts() {
+  public function load_lazysizes_scripts()
+  {
     // If it was loaded, skip it
     if (wp_script_is('lazysizes', 'enqueued')) {
       return;
     }
 
-    wp_register_script('lazysizes', CODETOT_OPTIMIZATION_URL . 'assets/lazysizes.min.js', array(), '5.2.2');
+    wp_register_script('lazysizes', CODETOT_OPTIMIZATION_URL . 'assets/lazysizes.min.js#async', array(), '5.2.2');
     wp_enqueue_script('lazysizes');
+  }
+
+  /**
+   * Replace #async with async="true" in script load
+   */
+  public function add_async_forscript($url)
+  {
+    if (strpos($url, '#async') === false) {
+      return $url;
+    } else if (is_admin()) {
+      return str_replace('#async', '', $url);
+    } else {
+      return str_replace('#async', '', $url) . "' async='async";
+    }
   }
 }
 
